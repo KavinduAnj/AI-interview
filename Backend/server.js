01464a64e32e851e50a58ce0b27e608305c50615
KavinduAnj <wkanjana1590@gmail.com>
2026-08-12 22:52:51 +0530

    db connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectDB = require("./config/db");
 app.use(cors());
 app.use(express.json());
+connectDB();
 
 app.get("/",(req, res) => {
     res.json({
@@ -10,7 +13,7 @@ app.get("/",(req, res) => {
     });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 });
