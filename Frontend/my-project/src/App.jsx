d4bdec5296bebd7a71cffe7e16815fccc4ba0c0f
KavinduAnj <wkanjana1590@gmail.com>
2026-09-08 18:50:31 +0530

    Authcontext added

--- Frontend/my-project/src/App.jsx
+++ Frontend/my-project/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
     return (
@@ -11,7 +12,10 @@ function App() {
                 
                 <Route path="/register" element={<Register />} />
                  <Route path="/login" element={<Login />} />
-                 <Route path="/dashboard" element={<Dashboard />} />
+                 <Route path="/dashboard" element={
+                        <ProtectedRoute>
+                            <Dashboard />
+                        </ProtectedRoute>} />
             </Routes>
         </BrowserRouter>
     );
---
+++ Frontend/my-project/src/context/Authcontext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+
+    const login = (userData, userToken) => {
+        setUser(userData);
+        setToken(userToken);
+
+        localStorage.setItem("user", JSON.stringify(userData));
+        localStorage.setItem("token", userToken);
+    }
+
+      const logout = () => {
+        setUser(null);
+        setToken(null);
+
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+    }
+     return (
+        <AuthContext.Provider
+            value={{
+                user,
+                token,
+                login,
+                logout
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+
+}
+export const useAuth = () => {
+    return useContext(AuthContext);
+}
